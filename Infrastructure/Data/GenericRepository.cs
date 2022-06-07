d248888e7188ef7a9e7ede01bc055b2fc2ab9185
rickyriosp <riki.rios91@gmail.com>
2022-06-07 14:49:50 -0400

    implementing repository with specification methods

--- Infrastructure/Data/GenericRepository.cs
+++ Infrastructure/Data/GenericRepository.cs
@@ -1,5 +1,6 @@
 using Core.Entities;
 using Core.Interfaces;
+using Core.Specifications;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data
@@ -23,6 +24,24 @@ namespace Infrastructure.Data
         {
             List<T> entities = await _context.Set<T>().ToListAsync();
             return entities;
+        }
+
+        public async Task<T> GetEntityWithSpec(ISpecification<T> spec)
+        {
+            T entity = await ApplySpecification(spec).FirstOrDefaultAsync();
+            return entity;
+        }
+
+        public async Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec)
+        {
+            List<T> entities = await ApplySpecification(spec).ToListAsync();
+            return entities;
+        }
+
+        private IQueryable<T> ApplySpecification(ISpecification<T> spec)
+        {
+            var query = SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(), spec);
+            return query;
         }
     }
 }
