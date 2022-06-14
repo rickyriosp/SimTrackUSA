e223ce065a3e091bc39580695c5741321db4dd5b
rickyriosp <riki.rios91@gmail.com>
2022-06-14 10:26:59 -0400

    adding HttpClientModule

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'SimTrackUSA';
+  products: any[];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe({
+      next: (response: any) => this.products = response.data,
+      error: err => console.log(err)
+    });
+  }
 }
