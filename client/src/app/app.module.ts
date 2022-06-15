168c833125008d99a009dff76d67075011b61268
rickyriosp <riki.rios91@gmail.com>
2022-06-15 09:56:30 -0400

    organizing client files and folders

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,15 +5,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { ShopModule } from './shop/shop.module';
 
 @NgModule({
-  declarations: [AppComponent, NavBarComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    CoreModule,
+    SharedModule,
+    ShopModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
