b5f5209dc4e9f1938a54e10f1bedfe2c3c219c3e
rickyriosp <riki.rios91@gmail.com>
2022-06-15 10:07:11 -0400

    add angular service and consume in component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,8 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
-import { IPagination } from './shared/models/pagination';
-import { IProduct } from './shared/models/product';
 
 @Component({
   selector: 'app-root',
@@ -11,14 +7,8 @@ import { IProduct } from './shared/models/product';
 })
 export class AppComponent implements OnInit {
   title = 'SimTrackUSA';
-  products: IProduct[];
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
-  ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe({
-      next: (response: IPagination) => (this.products = response.data),
-      error: (err) => console.log(err),
-    });
-  }
+  ngOnInit(): void {}
 }
--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -1,15 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
 import { ShopComponent } from './shop.component';
 
-
-
 @NgModule({
-  declarations: [
-    ShopComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ShopComponent],
+  imports: [CommonModule],
+  exports: [ShopComponent],
 })
-export class ShopModule { }
+export class ShopModule {}
