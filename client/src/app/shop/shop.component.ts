307de6b6a85734e9af606fbc72776ef82971e67e
rickyriosp <riki.rios91@gmail.com>
2022-06-16 17:16:15 -0400

    fixed bug - getProducts() trigger twice when applying filters - pager event is also triggered

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -77,8 +77,10 @@ export class ShopComponent implements OnInit {
   }
 
   onPageChanged(event: any) {
-    this.shopParams.pageNumber = event;
-    this.getProducts();
+    if (this.shopParams.pageNumber !== event) {
+      this.shopParams.pageNumber = event;
+      this.getProducts();
+    }
   }
 
   onSearch() {
