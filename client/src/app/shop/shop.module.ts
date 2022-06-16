26a2aff874b91fb8adae135ac2542de3aebfda5f
rickyriosp <riki.rios91@gmail.com>
2022-06-16 13:23:35 -0400

    create product component and passing input data to child components

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -2,9 +2,10 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { ShopComponent } from './shop.component';
+import { ProductItemComponent } from './product-item/product-item.component';
 
 @NgModule({
-  declarations: [ShopComponent],
+  declarations: [ShopComponent, ProductItemComponent],
   imports: [CommonModule],
   exports: [ShopComponent],
 })
