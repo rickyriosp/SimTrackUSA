1b526cb68267cef10312d9a25be7a60ad53b3738
rickyriosp <riki.rios91@gmail.com>
2022-06-19 11:20:12 -0400

    setup navigation links

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -1,14 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { SharedModule } from '../shared/shared.module';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ProductItemComponent } from './product-item/product-item.component';
 import { ShopComponent } from './shop.component';
-import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   declarations: [ShopComponent, ProductItemComponent, ProductDetailsComponent],
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, RouterModule, SharedModule],
   exports: [ShopComponent],
 })
 export class ShopModule {}
