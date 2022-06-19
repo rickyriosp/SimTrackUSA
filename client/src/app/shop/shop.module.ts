72e5a0e59542f6f482a9b858e33321cb42a48d94
rickyriosp <riki.rios91@gmail.com>
2022-06-19 10:56:47 -0400

    create home and product-details components

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { ProductItemComponent } from './product-item/product-item.component';
 import { ShopComponent } from './shop.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
-  declarations: [ShopComponent, ProductItemComponent],
+  declarations: [ShopComponent, ProductItemComponent, ProductDetailsComponent],
   imports: [CommonModule, SharedModule],
   exports: [ShopComponent],
 })
