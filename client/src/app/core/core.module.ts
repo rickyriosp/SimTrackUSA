6f316d00e1a8b1d7af1a697f83c39889102649ba
rickyriosp <riki.rios91@gmail.com>
2022-06-20 10:01:31 -0400

    error component for testing error responses

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { TestErrorComponent } from './test-error/test-error.component';
 
 @NgModule({
-  declarations: [NavBarComponent],
+  declarations: [NavBarComponent, TestErrorComponent],
   imports: [CommonModule, RouterModule],
   exports: [NavBarComponent],
 })
---
+++ client/src/app/core/test-error/test-error.component.ts
@@ -0,0 +1,52 @@
+import { environment } from 'src/environments/environment';
+
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-error',
+  templateUrl: './test-error.component.html',
+  styleUrls: ['./test-error.component.scss'],
+})
+export class TestErrorComponent implements OnInit {
+  baseUrl = environment.baseUrl;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {}
+
+  get400Error() {
+    this.http.get(this.baseUrl + 'buggy/badrequest').subscribe({
+      next: (response) => console.log(response),
+      error: (err) => console.log(err),
+    });
+  }
+
+  get400ValidationError() {
+    this.http.get(this.baseUrl + 'buggy/badrequest/fortytwo').subscribe({
+      next: (response) => console.log(response),
+      error: (err) => console.log(err),
+    });
+  }
+
+  get404Error() {
+    this.http.get(this.baseUrl + 'buggy/notfound').subscribe({
+      next: (response) => console.log(response),
+      error: (err) => console.log(err),
+    });
+  }
+
+  get500Error() {
+    this.http.get(this.baseUrl + 'buggy/servererror').subscribe({
+      next: (response) => console.log(response),
+      error: (err) => console.log(err),
+    });
+  }
+
+  get500MathError() {
+    this.http.get(this.baseUrl + 'buggy/math').subscribe({
+      next: (response) => console.log(response),
+      error: (err) => console.log(err),
+    });
+  }
+}
