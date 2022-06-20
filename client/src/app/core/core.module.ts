af50bc187b00851ba9433191dc147ca8a3d5d631
rickyriosp <riki.rios91@gmail.com>
2022-06-20 10:22:51 -0400

    create additional error components

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -4,9 +4,11 @@ import { RouterModule } from '@angular/router';
 
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { TestErrorComponent } from './test-error/test-error.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ServerErrorComponent } from './server-error/server-error.component';
 
 @NgModule({
-  declarations: [NavBarComponent, TestErrorComponent],
+  declarations: [NavBarComponent, TestErrorComponent, NotFoundComponent, ServerErrorComponent],
   imports: [CommonModule, RouterModule],
   exports: [NavBarComponent],
 })
