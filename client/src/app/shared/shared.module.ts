5ab69a8dbd3d2f27d9e590153834227ed3fed85e
rickyriosp <riki.rios91@gmail.com>
2022-06-21 15:45:36 -0400

    add home page content

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,14 +1,19 @@
-import { PaginationModule } from 'ngx-bootstrap/pagination';
-
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 import { PagerComponent } from './components/pager/pager.component';
 import { PagingHeaderComponent } from './components/paging-header/paging-header.component';
 
 @NgModule({
   declarations: [PagingHeaderComponent, PagerComponent],
-  imports: [CommonModule, PaginationModule.forRoot()],
-  exports: [PaginationModule, PagingHeaderComponent, PagerComponent],
+  imports: [CommonModule, PaginationModule.forRoot(), CarouselModule.forRoot()],
+  exports: [
+    PaginationModule,
+    PagingHeaderComponent,
+    PagerComponent,
+    CarouselModule,
+  ],
 })
 export class SharedModule {}
