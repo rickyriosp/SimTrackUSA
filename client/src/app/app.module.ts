c5f82c89a3e200dcb19651a14458ad2d06b9991b
rickyriosp <riki.rios91@gmail.com>
2022-06-21 14:21:39 -0400

    changing bootstrap theme

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import 'bootswatch/dist/simplex/bootstrap.min.css';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
