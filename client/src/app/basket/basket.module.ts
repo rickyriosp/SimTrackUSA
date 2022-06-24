6cd396788d9e345392192de9496823a98e020cc8
rickyriosp <riki.rios91@gmail.com>
2022-06-24 09:43:46 -0400

    create basket module

---
+++ client/src/app/basket/basket.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { BasketRoutingModule } from './basket-routing.module';
+import { BasketComponent } from './basket.component';
+
+@NgModule({
+  declarations: [BasketComponent],
+  imports: [CommonModule, BasketRoutingModule],
+})
+export class BasketModule {}
