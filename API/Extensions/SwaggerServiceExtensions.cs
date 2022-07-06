187de593011ff9b54937aec2df61f9f846f51034
rickyriosp <riki.rios91@gmail.com>
2022-07-06 13:28:04 -0400

    updating swagger config for identity

--- API/Extensions/SwaggerServiceExtensions.cs
+++ API/Extensions/SwaggerServiceExtensions.cs
@@ -9,6 +9,27 @@ public static class SwaggerServiceExtensions
         services.AddSwaggerGen(c =>
         {
             c.SwaggerDoc("v1", new OpenApiInfo { Title = "SimTrackUSA API", Version = "v1" });
+
+            var securitySchema = new OpenApiSecurityScheme
+            {
+                Description = "JWT Auth Bearer Scheme",
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.Http,
+                Scheme = "Bearer",
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            };
+            c.AddSecurityDefinition("Bearer", securitySchema);
+
+            var securityRequirement = new OpenApiSecurityRequirement
+            {
+                { securitySchema, new[] { "Bearer" } }
+            };
+            c.AddSecurityRequirement(securityRequirement);
         });
 
         return services;
